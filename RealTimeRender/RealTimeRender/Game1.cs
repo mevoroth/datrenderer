a8c5243f2783f95606bbd92cecdc90bb3be0a770
Daniel 'Mevoroth' Quach <darkskana@gmail.com>
2014-02-20 16:03:25 +0100

    gitignore

--- RealTimeRender/RealTimeRender/Game1.cs
+++ RealTimeRender/RealTimeRender/Game1.cs
@@ -26,6 +26,10 @@ namespace RealTimeRender
         float modelRotation = 0.0f;
 
         List<Model> listModel = new List<Model>();
+		List<Vector3> listModelCenter = new List<Vector3>();
+        // Set the position of the camera in world space, for our view matrix.
+        Vector3 cameraPosition = new Vector3(50.0f, 0.0f, 1700.0f);
+		Vector3 lookAt = Vector3.Zero;
 
         public Game1()
         {
@@ -51,12 +55,33 @@ namespace RealTimeRender
             ModelMiddleRes = Content.Load<Model>("mid_res");
             ModelHighRes = Content.Load<Model>("hight_res");
 
+			Vector3 center = _GetCenter(ModelHighRes);
+
             listModel.Add(ModelLowRes);
             listModel.Add(ModelMiddleRes);
             listModel.Add(ModelHighRes);
 
+			listModelCenter.Add(center);
+			listModelCenter.Add(center);
+			listModelCenter.Add(center);
         }
 
+		private Vector3 _GetCenter(Model model)
+		{
+			Vector3 center = Vector3.Zero;
+			int meshNum = 0;
+			foreach (ModelMesh m in model.Meshes)
+			{
+				center += m.BoundingSphere.Center;
+				++meshNum;
+			}
+			return center / (float)meshNum;
+		}
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
         protected override void UnloadContent()
         {
         }
@@ -68,6 +93,16 @@ namespace RealTimeRender
 
             if (kb.IsKeyDown(Keys.Down) == true)    camera.Distance += camera.Speed * (gameTime.ElapsedGameTime.Milliseconds / 1000f);
             if (kb.IsKeyDown(Keys.Up) == true)      camera.Distance -= camera.Speed * (gameTime.ElapsedGameTime.Milliseconds / 1000f);
+			if (kb.IsKeyDown(Keys.Left))
+			{
+				cameraPosition.X -= 25.0f;
+				lookAt.X -= 25f;
+			}
+			if (kb.IsKeyDown(Keys.Right))
+			{
+				cameraPosition.X += 25.0f;
+				lookAt.X += 25f;
+			}
 
             modelRotation += (float)gameTime.ElapsedGameTime.TotalMilliseconds * MathHelper.ToRadians(0.01f);
             base.Update(gameTime);
@@ -84,10 +119,8 @@ namespace RealTimeRender
             Model tempModel;
 
             // active DepthBuffer
-			DepthStencilState dss = new DepthStencilState();
-			dss.DepthBufferEnable = true;
-			dss.DepthBufferWriteEnable = true;
-			graphics.GraphicsDevice.DepthStencilState = dss;
+			//graphics.GraphicsDevice.DepthStencilState.DepthBufferEnable = true;
+            
 
             // Level Of Detail
             if (camera.Position.Z > 1500.0f)
@@ -97,10 +130,37 @@ namespace RealTimeRender
             else
                 tempModel = ModelHighRes;
 
+			// Resolution by angle
+			Vector3 objCenter = listModelCenter[listModel.IndexOf(tempModel)];
+			Vector3 d1 = (objCenter - cameraPosition);
+			d1.Normalize();
+			Vector3 d2 = lookAt - cameraPosition;
+			d2.Normalize();
+			float diff = Vector3.Dot(d1, d2);
+			spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+			spriteBatch.DrawString(font, "  Angle = " + diff.ToString(), new Vector2(25.0f, 50f), Color.White);
+			spriteBatch.End();
+			if (diff > 0.9f)
+			{
+				tempModel = ModelHighRes;
+			}
+			else if (diff > 0.7f)
+			{
+				tempModel = ModelMiddleRes;
+			}
+			else
+			{
+				tempModel = ModelLowRes;
+			}
+			//graphics.GraphicsDevice.RenderState.DepthBufferEnable = true;
+			DepthStencilState dss = new DepthStencilState();
+			dss.DepthBufferEnable = true;
+			dss.DepthBufferWriteEnable = true;
+			graphics.GraphicsDevice.DepthStencilState = dss;
             // Copy any parent transforms.
             Matrix[] transforms = new Matrix[tempModel.Bones.Count];
             tempModel.CopyAbsoluteBoneTransformsTo(transforms);
-
+			
             // Draw the model. A model can have multiple meshes, so loop.
             foreach (ModelMesh mesh in tempModel.Meshes)
             {
@@ -110,7 +170,7 @@ namespace RealTimeRender
                     effect.EnableDefaultLighting();
                     effect.World = transforms[mesh.ParentBone.Index] * Matrix.CreateRotationY(modelRotation)
                         * Matrix.CreateTranslation(modelPosition);
-                    effect.View = Matrix.CreateLookAt(camera.Position, Vector3.Zero, Vector3.Up);
+                    effect.View = Matrix.CreateLookAt(cameraPosition, lookAt, Vector3.Up);
                     effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f),
                         camera.AspectRatio, 1.0f, 10000.0f);
                 }
